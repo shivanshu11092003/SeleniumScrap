8bf203557efb28550ee893f5e37d9fbfe58fbc1f
shivanshu11092003 <sshivanshugupta11@gmail.com>
2024-11-13 02:30:38 +0530

    Task Complete

--- Scrap.py
+++ Scrap.py
@@ -7,7 +7,7 @@ import time
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver import ActionChains
 from bs4 import BeautifulSoup
-
+import xlsxwriter
 
 
 
@@ -25,12 +25,15 @@ driver.get("https://tech.kiet.edu/team-erp/")
 driver.implicitly_wait(9)
 
 
-linkedinArray = []
 try:
+  linkedinArray = []
+  education =[]
+  experience =[]
 
 
 
-  for i in range(2,3):
+
+  for i in range(2,4):
     a = driver.find_elements(by=By.XPATH,value=f"(//section)[{i}]/div/div/div")
     for item in a:
       actions = ActionChains(driver)
@@ -51,11 +54,55 @@ try:
       linkedinArray.append(finalLink)
   print(linkedinArray)
 
-  for newItem in linkedinArray:
-     newLink =  driver.get(f"{newItem}")
+  newLink =  driver.get("https://www.linkedin.com/checkpoint/lg/sign-in-another-account")
+  driver.implicitly_wait(5)
+  username = driver.find_element(By.ID,"username")
+  username.click()
+  username.send_keys("sshivanshugupta11@gmail.com")
+  password = driver.find_element(By.ID,"password")
+  password.click()
+  password.send_keys("Shivanshu@_123")
+  btn = driver.find_element(By.TAG_NAME,"button")
+  btn.click()
+  time.sleep(4)
+  for item in linkedinArray:
+     driver.get(f"{item}")
+     driver.implicitly_wait(5)
 
 
-  # driver.implicitly_wait(15)
+
+
+     section = driver.find_element(By.ID,"education")
+     parentSectionEdu = section.find_element(By.XPATH,"..")
+     education.append(parentSectionEdu.text)
+ 
+
+
+
+     sectionExp = driver.find_element(By.ID,"experience")
+     parentSectionExp = sectionExp.find_element(By.XPATH,"..")
+     experience.append(parentSectionExp.text)
+  workbook = xlsxwriter.Workbook('complete.xlsx')
+  worksheet = workbook.add_worksheet()
+  row = 0
+  for i in linkedinArray:
+     worksheet.write(row, 0, i)
+     worksheet.write(row, 1, education[row])
+     worksheet.write(row, 2, experience[row])
+     row += 1
+  
+  workbook.close()
+  
+
+     
+
+
+
+
+
+
+  # for newItem in linkedinArray:
+
 
 
   # # send_keys("manuisgreat9@gmail.com")
